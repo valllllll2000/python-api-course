f5736031f4d9a9d0662ee4c3fcad4ffe343f268b
valllllll2000 <valerie.cardoso@gmail.com>
2024-08-15 21:58:33 +0200

    fix typo

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,7 +9,7 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
-class Comand(BaseCommand):
+class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
