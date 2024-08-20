7f89cf0d3f88b6df6084bdb316e3f883a54c17f6
valllllll2000 <valerie.cardoso@gmail.com>
2024-08-20 15:44:57 +0200

    fixed lint errors

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,11 +5,12 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
     class Meta:
-        model =  get_user_model()
+        model = get_user_model()
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
